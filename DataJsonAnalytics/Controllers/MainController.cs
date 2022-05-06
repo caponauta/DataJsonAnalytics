adbdb9859fa30bd2a6039c2bb9e838cbc4eb0dfb
Nicolás López <braianlopez3941@gmail.com>
2022-05-06 11:19:14 -0500

    Ajuste Final Servicio Lst

--- DataJsonAnalytics/Controllers/MainController.cs
+++ DataJsonAnalytics/Controllers/MainController.cs
@@ -1,7 +1,7 @@
 ﻿using DataJsonAnalytics.Models;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Npgsql;
+using System.Collections.Generic;
 using System.Net;
 using System.Net.Http;
 
@@ -13,68 +13,71 @@ namespace DataJsonAnalytics.Controllers
     {
         [Route("save-data")]
         [HttpPost]
-        public HttpResponseMessage SaveData(Conversation conversation)
+        public HttpResponseMessage SaveData(List<Conversation> lstConversation)
         {
             HttpResponseMessage httpResponseMessage = new HttpResponseMessage();
 
             try
             {
-                if (conversation != null)
+                foreach (var conversation in lstConversation)
                 {
-                    // Guardar registros
-                    // conversation
-                    string QueryConversation = "insert into conversations values ('" + conversation.conversationId + "', '" + conversation.conversationStart.ToString("yyyy-MM-ddTHH:mm:ss.FFFZ") + "' ,'" + conversation.conversationEnd.ToString("yyyy-MM-ddTHH:mm:ss.FFFZ") + "'," + conversation.mediaStatsMinConversationMos.ToString().Replace(",", ".") + "," + conversation.mediaStatsMinConversationRFactor.ToString().Replace(",", ".") + ",'" + conversation.originatingDirection + "','" + conversation.divisionIds[0] + "')";
-                    SaveDataAdo(QueryConversation);
-
-                    if (conversation.participants.Count > 0)
+                    if (conversation != null)
                     {
-                        foreach (var itemParticipant in conversation.participants)
+                        // Guardar registros
+                        // conversation
+                        string QueryConversation = "insert into conversations values ('" + conversation.conversationId + "', '" + conversation.conversationStart.ToString("yyyy-MM-ddTHH:mm:ss.FFFZ") + "' ,'" + conversation.conversationEnd.ToString("yyyy-MM-ddTHH:mm:ss.FFFZ") + "'," + conversation.mediaStatsMinConversationMos.ToString().Replace(",", ".") + "," + conversation.mediaStatsMinConversationRFactor.ToString().Replace(",", ".") + ",'" + conversation.originatingDirection + "','" + conversation.divisionIds[0] + "')";
+                        SaveDataAdo(QueryConversation);
+
+                        if (conversation.participants.Count > 0)
                         {
-                            string QueryParticipants = "insert into participants values ('" + itemParticipant.participantId + "', '" + conversation.conversationId + "','" + itemParticipant.participantName + "','" + itemParticipant.purpose + "','" + itemParticipant.userId + "');";
-                            SaveDataAdo(QueryParticipants);
-
-                            if (itemParticipant.sessions.Count > 0)
+                            foreach (var itemParticipant in conversation.participants)
                             {
-                                foreach (var itemSession in itemParticipant.sessions)
+                                string QueryParticipants = "insert into participants values ('" + itemParticipant.participantId + "', '" + conversation.conversationId + "','" + itemParticipant.participantName + "','" + itemParticipant.purpose + "','" + itemParticipant.userId + "');";
+                                SaveDataAdo(QueryParticipants);
+
+                                if (itemParticipant.sessions.Count > 0)
                                 {
-                                    string QuerySession = "insert into sessions values ('" + itemSession.sessionId + "', '" + itemParticipant.participantId + "','" + itemSession.mediaType + "','" + itemSession.direction + "', '" + itemSession.peerId + "', '" + itemSession.provider + "', '" + itemSession.requestedRoutings[0] + "','" + itemSession.selectedAgentId + "','" + itemSession.remote + "');";
-                                    SaveDataAdo(QuerySession);
-
-                                    if (itemSession.segments.Count > 0)
+                                    foreach (var itemSession in itemParticipant.sessions)
                                     {
-                                        foreach (var itemSegment in itemSession.segments)
-                                        {
-                                            // segment_id is session_id + _ + segment_start + _ + segment_end
-                                            string segment_id = itemSession.sessionId + "_" + itemSegment.segmentStart + "_" + itemSegment.segmentEnd;
+                                        string QuerySession = "insert into sessions values ('" + itemSession.sessionId + "', '" + itemParticipant.participantId + "','" + itemSession.mediaType + "','" + itemSession.direction + "', '" + itemSession.peerId + "', '" + itemSession.provider + "', '" + (itemSession.requestedRoutings == null ? "" : itemSession.requestedRoutings[0]) + "','" + (System.String.IsNullOrEmpty(itemSession.selectedAgentId) ? "" : itemSession.selectedAgentId) + "','" + itemSession.remote + "');";
+                                        SaveDataAdo(QuerySession);
 
-                                            //Insert segmento
-                                            string QuerySegment = "insert into segments values ('" + segment_id + "','" + itemSession.sessionId + "','" + itemSegment.segmentStart.ToString("yyyy-MM-ddTHH:mm:ss.FFFZ") + "','" + itemSegment.segmentEnd.ToString("yyyy-MM-ddTHH:mm:ss.FFFZ") + "','" + itemSegment.segmentType + "','" + itemSegment.conference + "','" + itemSegment.disconnectType + "','" + itemSegment.queueId + "');";
-                                            SaveDataAdo(QuerySegment);
+                                        if (itemSession.segments.Count > 0)
+                                        {
+                                            foreach (var itemSegment in itemSession.segments)
+                                            {
+                                                // segment_id is session_id + _ + segment_start + _ + segment_end
+                                                string segment_id = itemSession.sessionId + "_" + itemSegment.segmentStart + "_" + itemSegment.segmentEnd;
+
+                                                //Insert segmento
+                                                string QuerySegment = "insert into segments values ('" + segment_id + "','" + itemSession.sessionId + "','" + itemSegment.segmentStart.ToString("yyyy-MM-ddTHH:mm:ss.FFFZ") + "','" + itemSegment.segmentEnd.ToString("yyyy-MM-ddTHH:mm:ss.FFFZ") + "','" + itemSegment.segmentType + "','" + itemSegment.conference + "','" + itemSegment.disconnectType + "','" + itemSegment.queueId + "');";
+                                                SaveDataAdo(QuerySegment);
+                                            }
                                         }
-                                    }
 
-                                    if (itemSession.metrics.Count > 0)
-                                    {
-                                        foreach (var itemMetric in itemSession.metrics)
+                                        if (itemSession.metrics.Count > 0)
                                         {
-                                            // metric_id is session_id + _ + name + _ + value
-                                            string metric_id = itemSession.sessionId + "_" + itemMetric.name + "_" + itemMetric.value;
+                                            foreach (var itemMetric in itemSession.metrics)
+                                            {
+                                                // metric_id is session_id + _ + name + _ + value
+                                                string metric_id = itemSession.sessionId + "_" + itemMetric.name + "_" + itemMetric.value;
 
-                                            //Insert metricas
-                                            string QueryMetrics = "insert into metrics values ('" + metric_id + "','" + itemSession.sessionId + "','" + itemMetric.name + "'," + itemMetric.value + ",'" + itemMetric.emitDate.ToString("yyyy-MM-ddTHH:mm:ss.FFFZ") + "')";
-                                            SaveDataAdo(QueryMetrics);
+                                                //Insert metricas
+                                                string QueryMetrics = "insert into metrics values ('" + metric_id + "','" + itemSession.sessionId + "','" + itemMetric.name + "'," + itemMetric.value + ",'" + itemMetric.emitDate.ToString("yyyy-MM-ddTHH:mm:ss.FFFZ") + "')";
+                                                SaveDataAdo(QueryMetrics);
+                                            }
                                         }
                                     }
                                 }
                             }
                         }
-                    }
 
-                    httpResponseMessage.StatusCode = HttpStatusCode.OK;
-                }
-                else
-                {
-                    httpResponseMessage.StatusCode = HttpStatusCode.NotFound;
+                        httpResponseMessage.StatusCode = HttpStatusCode.OK;
+                    }
+                    else
+                    {
+                        httpResponseMessage.StatusCode = HttpStatusCode.NotFound;
+                    }
                 }
             }
             catch (System.Exception)
@@ -90,21 +93,20 @@ namespace DataJsonAnalytics.Controllers
             try
             {
                 var connection = "Host=localhost;Username=postgres;Password=asd123;Database=JsonDataExample";
+                //var connection = "Host=190.168.0.18:5432;Username=dbuser;Password=c0nversat1onMod3lToSql;Database=conversations_db";
 
                 NpgsqlConnection con = new NpgsqlConnection(connection);
                 con.Open();
 
                 var cmd = new NpgsqlCommand();
                 cmd.Connection = con;
-                //cmd.CommandText = "insert into conversations values ('" + conversation.conversationId + "', '" + conversation.conversationStart.ToString("yyyy-MM-ddTHH:mm:ss.F") + "' ,'" + conversation.conversationEnd.ToString("yyyy-MM-ddTHH:mm:ss.F") + "'," + conversation.mediaStatsMinConversationMos + "," + conversation.mediaStatsMinConversationRFactor + ",'" + conversation.originatingDirection + "','" + conversation.divisionIds[0] + "')";
                 cmd.CommandText = Command;
                 cmd.ExecuteNonQuery();
 
                 con.Close();
             }
-            catch (System.Exception)
+            catch (System.Exception ex)
             {
-
                 throw;
             }
         }
